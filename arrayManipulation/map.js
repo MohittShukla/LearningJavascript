3b587fe974a058ee450c4939fcc7ae3eeda2605f
Mohit <mohit@Apples-MacBook-Air.local>
2025-04-08 16:38:45 +0530

    adding comments in map.js

--- arrayManipulation/map.js
+++ arrayManipulation/map.js
@@ -1,61 +1,69 @@
-// you have to return something in map otherwise it returns an array with undefined elements
+// If you're using map, make sure you return something inside it,
+// otherwise you'll end up with an array full of undefined
+
+console.log('');
+
+// Array of numbers
+const number = [1, 2, 3, 4, 5, 6];
+
+// Adding 10 to each number using map
+const sum = number.map(options => {
+    return options + 10;
+});
+
+console.log("Each number +10 gives:");
+console.log(sum);
+console.log('');
 
 
-console.log('')
-
-const number = [1,2,3,4,5,6];
-// adding 10 to each number using map;
-
-
- const sum = number.map(options => {
-    options = options + 10;
-    return options;
- })
-
-console.log(`10 more is -> ${sum}`);
-console.log('')
-
-
+// Array of fruit names
 const fruits = ['apple', 'banana', 'mango'];
-// making it uppercase;
+
+// Converting all fruit names to uppercase
 const big = fruits.map(i => i.toUpperCase());
+
+console.log("Fruits in uppercase:");
 console.log(big);
-console.log('')
+console.log('');
 
-// Get the lengths of each string
 
+// Getting length of each fruit name
 const len = fruits.map(l => {
     return l.length;
-})
+});
+
+console.log("Length of each fruit name:");
 console.log(len);
+console.log('');
 
-console.log('')
 
-// Extract names from array of objects
-
+// Extracting names from array of user objects
 const users = [
     { name: 'Alice', age: 22 },
     { name: 'Bob', age: 25 },
     { name: 'Charlie', age: 30 }
-  ];
+];
 
-
-  const names = users.map(i => {
+// Getting only the names from the users array
+const names = users.map(i => {
     return i.name;
-  })
+});
 
-  console.log(names);
-  console.log('')
+console.log("Names of all users:");
+console.log(names);
+console.log('');
 
 
-//   Format strings from object
-  const products = [
+// Formatting product names with prices
+const products = [
     { name: 'Laptop', price: 1000 },
     { name: 'Phone', price: 500 },
-  ];
+];
 
-  const formatted = products.map(format =>{
-    return  `${format.name} costs ${format.price}`;
-  })
+// Creating formatted strings from product details
+const formatted = products.map(format => {
+    return `${format.name} costs ${format.price}`;
+});
 
-  console.log(formatted);
+console.log("Formatted product info:");
+console.log(formatted);
